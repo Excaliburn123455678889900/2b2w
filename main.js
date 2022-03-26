2c8c2ed6fbe73278ddb6e8b70204a64e8b5d0d79
Excaliburn123455678889900 <93167410+Excaliburn123455678889900@users.noreply.github.com>
2022-03-27 09:33:47 +1100

    Update Main.js

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ const askForSecrets = async () => {
 	}
 	if (localConf.discordBot) {
 		dc = new discord.Client();
-		dc.login(discordBotToken??config.get('BotToken')).catch(()=>{
+		dc.login("OTE1NDkzMDc0MzIxNDMyNTc5.YacZXQ.kXL5ZEkV1TyWLIzzrCK0Q8G-9u4").catch(()=>{
 			console.warn("There was an error when trying to log in using the provided Discord bot token. If you didn't enter a token this message will go away the next time you run this program!"); //handle wrong tokens gracefully
 		});
 		dc.on('ready', () => {
